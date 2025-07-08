4e2c00fd44f20d2e2425718c6889a7c2c04ff168
JosueRodLop <josrodlop19@alum.us.es>
2025-07-08 21:24:48 +0200

    fix: delete duplicated code

--- vulnerableCodeExamples/jacksonDatabind-CWE-502/src copy/main/java/com/example/User.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import java.io.Serializable;
-
-public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public int id;
-    public String name;
-}
