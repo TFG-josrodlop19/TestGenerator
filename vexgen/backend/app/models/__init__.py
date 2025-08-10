4e959e9726e5a9e425ff1f0af05326771da9a109
JosueRodLop <josrodlop19@alum.us.es>
2025-08-10 12:15:28 +0200

    fix: reinstall vexgen

---
+++ vexgen/backend/app/models/__init__.py
@@ -0,0 +1,17 @@
+from .models import (
+    AccountExistsRequest,
+    ChangePasswordRequest,
+    GenerateVEXRequest,
+    LoginRequest,
+    User,
+    VerifyAccessTokenRequest,
+)
+
+__all__ = [
+    "AccountExistsRequest",
+    "ChangePasswordRequest",
+    "LoginRequest",
+    "User",
+    "VerifyAccessTokenRequest",
+    "GenerateVEXRequest",
+]
