8a8146d7173c542a04f8b61d6c6e7ba3b08d1f11
JosueRodLop <josrodlop19@alum.us.es>
2025-08-21 23:06:12 +0200

    feat: new function to generate vex

--- src/test_file.py
+++ src/test_file.py
@@ -2,10 +2,12 @@ from java_analyzer.function_identifier import open_java_file, analyze_java_file
 import javalang
 from typing import Dict, Optional
 from vexgen_caller.auth import signup, login
+from vexgen_caller.vex_generator import generate_vex
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__== "__main__":
     # login("test@example.com", "Pa$$word123")
-    signup("test2@example.com", "Pa$$word123")
+    # signup("test2@example.com", "Pa$$word123")
+    generate_vex("depexorg", "vex_generation", "sbom.json")
--- src/vexgen_caller/vex_generator.py
+++ src/vexgen_caller/vex_generator.py
@@ -1,3 +1,36 @@
-import utils.request_helper as request
+import os
+import requests
+import json
 
-def generate_vex()
+
+def generate_vex(owner:str, name:str, sbom_path:str):
+    url = os.getenv("VEXGEN_URL") + "/vex/generate"
+    if not owner or not name or not sbom_path:
+        raise ValueError("Owner, name and SBOM path must be provided for VEX generation.")
+    # Get user ID from json file
+    token_file = os.path.expanduser(os.getenv("VEXGEN_TOKEN_FILE"))
+    if not os.path.exists(token_file):
+        raise FileNotFoundError(f"Token file not found: {token_file}")
+    with open(token_file, 'r') as f:
+        token_data = json.load(f)
+        token = token_data.get("token")
+        user_id = token_data.get("user_id")
+        if not user_id:
+            raise ValueError("User ID must be provided or found in the token file.")
+        if not token:
+            raise ValueError("Token must be provided or found in the token file.")
+    data = {
+        "owner": owner,
+        "name": name,
+        "sbom_path": sbom_path,
+        "statements_group": "no_grouping",
+        "user_id": user_id
+    }
+    
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()
+        print("Vex generation successful.")
+        # print(response.json())
+    except requests.RequestException as e:
+        print(f"Error generating VEX: {response.json().get('message', 'Unknown error')}")
