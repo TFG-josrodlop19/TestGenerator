355ddb86199bc6bce50cc55db76647b7eca2a677
JosueRodLop <josrodlop19@alum.us.es>
2025-08-27 20:50:03 +0200

    feat: add fucntion to write info about tests generation and execution

--- src/utils/file_writer.py
+++ src/utils/file_writer.py
@@ -30,7 +30,6 @@ def write_token_to_file(token: str, user_id: str = None):
     except Exception as e:
         print(f"Failed to write token to file: {e}")
         
-        
 
 def os_path_separators():
     seps = []
@@ -126,5 +125,22 @@ def generate_path_repo(owner:str, name:str) -> str:
     
     folder_name = f"{owner}_{name}"
     project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    general_vex_path = os.path.join(project_root, "OSS-Fuzz", "projects")
-    return make_valid_file_path(folder_name, general_vex_path)
+    repo_path = os.path.join(project_root, "OSS-Fuzz", "projects")
+    return make_valid_file_path(folder_name, repo_path)
+
+
+def generate_test_info_path(owner:str, name:str) -> str:
+    folder_name = f"{owner}/{name}"
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    info_path = os.path.join(project_root, "tests_info")
+    return make_valid_file_path(folder_name, info_path)
+
+def write_test_info_to_json(owner:str, name:str, info:dict):
+    info_path = generate_test_info_path(owner, name)
+    
+    os.makedirs(info_path, exist_ok=True)
+    
+    file_path = os.path.join(info_path, "tests_info.json")
+    
+    with open(file_path, "w") as f:
+        json.dump(info, f, indent=4)
